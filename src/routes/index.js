c82bcbf0e0ea02db47aa107c91139abeedc1b945
caidong32 <caidong32@gmail.com>
2022-04-20 10:39:32 +0800

    set local db.

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // Connection DB
 var connection = mysql.createConnection({
-    host:'content-auto-prod.cluster-cwvtgy0jxm2o.ap-northeast-2.rds.amazonaws.com', 
+    host:'127.0.0.1', 
     user:'auto_dev', 
     password:'auto_dev12#', 
     port:3306, 
