94b55b850f595a326d93382e1481b5238afcf48e
caidong32 <caidong32@gmail.com>
2022-04-20 16:53:14 +0800

    db

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,6 @@
 var express = require('express');
-var mysql = require('mysql');
 var router = express.Router();
-
-// Connection DB
-var connection = mysql.createConnection({
-    host: 'content-auto-prod.cluster-cwvtgy0jxm2o.ap-northeast-2.rds.amazonaws.com', 
-    user: 'auto_dev', 
-    password: 'auto_dev12#', 
-    port: 3306, 
-    database: 'automobile_dev' 
-});
-
-// Connection
-connection.connect(function (err) {
-    if(err) {
-        console.error(err.message);
-        return;
-    }
-    console.error('database connected.');
-});
+var connection = require('../database');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -26,10 +8,15 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/list', function(req, res, next) {
-	connection.query('SELECT * FROM backend_test_table', function (err, rows) {
-		if(err) throw err;
-		res.send(rows);
-	});
+    connection.query('SELECT * FROM backend_test_table', (error, rows, fields) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
+        }
+    
+        console.log(': ', rows);
+        res.send(rows);
+    });
 });
 
 module.exports = router;
