677d23cc3c9f098818c871d0d812d564bfd47ee6
caidong32 <caidong32@gmail.com>
2022-04-20 16:38:38 +0800

    db

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,20 +4,20 @@ var router = express.Router();
 
 // Connection DB
 var connection = mysql.createConnection({
-    host: 'automobile_dev.content-auto-prod.cluster-cwvtgy0jxm2o.ap-northeast-2.rds.amazonaws.com', 
+    host: 'content-auto-prod.cluster-cwvtgy0jxm2o.ap-northeast-2.rds.amazonaws.com', 
     user: 'auto_dev', 
     password: 'auto_dev12#', 
-    port: 3306, 
+    port: '3306', 
     database: 'automobile_dev' 
 });
 
 // Connection
 connection.connect(function (err) {
     if(err) {
-        console.error('mysql connection error');
-        console.error(err);
-        throw err;
+        console.error(err.message);
+        return;
     }
+    console.error('database connected.');
 });
 
 /* GET home page. */
