bb2a9e3cb90b9afb26ee43580d6d4f02a32dd05d
caidong32 <caidong32@gmail.com>
2022-04-20 16:55:27 +0800

    db

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ var logger = require('morgan');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var indexRouter = require('./routes/index');
-var mysql = require('mysql');
 var app = express();
 
 app.use(logger('dev'));
@@ -10,23 +9,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/api', indexRouter);
-
-// Connection DB
-var connection = mysql.createConnection({
-    host:'zum-auto-production.cluster-cwvtgy0jxm2o.ap-northeast-2.rds.amazonaws.com', 
-    user:'fastview', 
-    password:'daily-!hero-!!1-cat!-!seoul!', 
-    port:3306, 
-    database:'zum-auto-server_production' 
-});
-
-// Connection
-connection.connect(function (err) {
-    if(err) {
-        console.error('mysql connection error');
-        console.error(err);
-        throw err;
-    }
-});
 
 module.exports = app;
