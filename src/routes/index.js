dac2018c577b5b4e80408d8c88c2067a20ff873c
caidong32 <caidong32@gmail.com>
2022-04-20 16:40:11 +0800

    db

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     host: 'content-auto-prod.cluster-cwvtgy0jxm2o.ap-northeast-2.rds.amazonaws.com', 
     user: 'auto_dev', 
     password: 'auto_dev12#', 
-    port: '3306', 
+    port: 3306, 
     database: 'automobile_dev' 
 });
 
