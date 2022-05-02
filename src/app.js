ca6d89a23cedc630d25acc915fc822f442b3afc9
caidong32 <caidong32@gmail.com>
2022-05-02 21:41:16 +0800

    cors

--- src/app.js
+++ src/app.js
@@ -24,8 +24,19 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({origin: `*`}));
-app.options('*', cors());
+
+app.use(cors({
+    origin:['http://localhost:4200','http://127.0.0.1:4200'],
+    credentials:true
+}));
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Headers', true);
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  next();
+});
 
 app.use('/api', indexRouter);
 app.use('/test', testRouter);
