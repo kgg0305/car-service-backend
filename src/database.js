ae1b434f1df68957fdec244536c42182159fe6aa
caidong32 <caidong32@gmail.com>
2022-05-02 11:44:01 +0800

    db

--- src/database.js
+++ src/database.js
@@ -1,21 +1,21 @@
 var mysql = require('mysql');
 
 // Connection DB
-// var connection = mysql.createConnection({
-//     host: 'content-auto-prod.cluster-cwvtgy0jxm2o.ap-northeast-2.rds.amazonaws.com', 
-//     user: 'auto_dev', 
-//     password: 'auto_dev12#', 
-//     port: 3306, 
-//     database: 'automobile_dev' 
-// });
-
 var connection = mysql.createConnection({
-    host: '127.0.0.1', 
-    user: 'root', 
-    password: '', 
+    host: 'content-auto-prod.cluster-cwvtgy0jxm2o.ap-northeast-2.rds.amazonaws.com', 
+    user: 'auto_dev', 
+    password: 'auto_dev12#', 
     port: 3306, 
     database: 'automobile_dev' 
 });
+
+// var connection = mysql.createConnection({
+//     host: '127.0.0.1', 
+//     user: 'root', 
+//     password: '', 
+//     port: 3306, 
+//     database: 'automobile_dev' 
+// });
 
 // Connection
 connection.connect(function (err) {
